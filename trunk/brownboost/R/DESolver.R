06d66bd6c696dfb661e5d285334b18396ca69c4c
davebot1 <davebot1@c1a95de8-36a4-8094-8df2-00efaedc29e3>
2010-04-30 23:14:08 +0000

    trying new solver

--- trunk/brownboost/R/DESolver.R
+++ trunk/brownboost/R/DESolver.R
@@ -13,8 +13,9 @@ dotl <- function (v, l) {
 # in the papers ... it's 2/pi instead of 2/sqrt(pi)...
 
 erfd <- function(a) {
-  #print(a[1])
-  return((1/pi)*as.double(erf(a)))
+  #cat("min a: ", min(a), "\n")
+  #return((1/pi)*as.double(erf(a)))
+  return(2*pnorm(a*sqrt(2)) - 1)
 }
 
 # when the solution is found, this result should be c(0,0)
@@ -64,7 +65,8 @@ updateStep <- function(alpha, tee, c, W, U, B, V, E) {
     a2 <- alpha + (c*Bk + sqrt(pi*c)*Uk*(E/W)) / (2*(Vk - Uk*Bk))
     t2 <- tee + (c*Bk^2 + sqrt(pi*c)*Vk*(E/W)) / (2*(Vk - Uk*Bk))
   } else {
-    a2 = t2 = 0
+    a2 = alpha
+    t2 = tee
   }
   return(data.frame(alpha=a2, tee=t2))
 }
@@ -81,11 +83,7 @@ solvede <- function(r, s, h, y, c) {
   boundry <- boundryCondition(a, b, v, z, c)
   lastBoundry <- 0
   # run until the variables stop changing...
-  while (abs(sum(lastBoundry) - sum(boundry)) > 1e-12) {
-    #print("this boundry")
-    #print(sum(boundry))
-    #print("last boundry")
-    #print(sum(lastBoundry))
+  while (abs(sum(lastBoundry) - sum(boundry)) > 1e-11) {
     j <- jacobianElements(a, b, v, z, c)
     update <- updateStep(alpha, tee, c, j$W, j$U, j$B, j$V, j$E)
     z_last <- z
@@ -93,6 +91,10 @@ solvede <- function(r, s, h, y, c) {
     #v <- list(b, -1)
     lastBoundry <- boundry
     boundry <- boundryCondition(a, b, v, z, c)
+    print("last boundry")
+    print((lastBoundry))
+    print("new boundry")
+    print(boundry)
     # Check boundry condition ... time to quit?
   }
   #print("Done with solving")
@@ -105,19 +107,17 @@ solvede <- function(r, s, h, y, c) {
 # OK so here,
 #  a == r(x_j, y_j) + s_i :  The margin + the step s
 #  and b == h(x_i) * y_i  :  The hypothesis * the prediction
+#  run by solvede(r, s, h, y, c) 
 runSolverTest1 <- function() {
-  alpha <- 0.0
-  tee <- 0.0
   epsilon <- 0.005                              # Estimated error of data set
-  c <- (1/as.double(erfc(1-epsilon)))^2         # c defined from epsilon
-  s <- c                                        # s == c at t == 0
-  a <-rep(0, times=500) + s                     # This is the margin r(x) + step
+  c <- 4                                        # c can be defined from epsilon
+  s <- c                                        # step
+  r <- rep(0, times=500)                        # This is the margin r(x)
   h <- sample(x=c(-1,1), replace=T, size=500)   # This is the hypothesis
   y <- h                                        # This is the true class
   i = sample(x=seq(1, 500), replace=F, size=150)
   y[i] <- -1 * y[i]                             # Give 30% error to y(x)
-  b <- h * y                                    # agreement == 1, disagreement == -1
-  result <- solvede(alpha, tee, a, b, c)
+  result <- solvede(r, s, h, y, c)
   print(result)
 }
 

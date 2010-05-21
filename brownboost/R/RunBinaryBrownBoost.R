fbe237bd763def7cf27ddfd3857fb4d5fac49bff
davebot1 <davebot1@c1a95de8-36a4-8094-8df2-00efaedc29e3>
2010-05-21 15:57:30 +0000

    fixed the problem ... data format

--- brownboost/R/RunBinaryBrownBoost.R
+++ brownboost/R/RunBinaryBrownBoost.R
@@ -1,9 +1,15 @@
 source("brownboost/R/BinaryBrownBoost.R", local=T)
 
 runBinaryBrownBoost <- function (trainingData, testData, c) {
+
+  print("testdata")
+  print(testData$Class)
+  print("traindata")
+  print(trainingData$Class)
   
   ensemble <- bbBuildEnsemble(trainingData, c)
   results  <- bbRunEnsemble(ensemble, testData)
+
   cat("Number of Stumps used: ", length(ensemble[[1]]), "\n")
   cat("Accuracy: ", bbAccuracy(results, testData$Class), "\n")
   print("Confusion Matrix - rows are true classes, cols are predictions")
@@ -12,16 +18,10 @@ runBinaryBrownBoost <- function (trainingData, testData, c) {
 }
 
 
-randomBinaryBrownBoost <- function (data, c, v=NULL) {
-  # v is the sample to sample from
-  if(is.null(v)) {
-    print("creating sample vector")
-    v <- 1:length(data[,1])
-  }
-  sampleSize <- 0.75 * length(v)
-  trainDex <- sample(v, replace=F, size=sampleSize)
-  print(trainDex)
+randomBinaryBrownBoost <- function (data, c) {
+  v <- 1:length(data$Class)
+  sampleSize <- 0.80 * length(v)
+  trainDex <- sample(x=v, replace=F, size=sampleSize)
   testDex <- which(!v %in% trainDex)
-  print(testDex)
   runBinaryBrownBoost(data[trainDex,], data[testDex,], c)
 }
